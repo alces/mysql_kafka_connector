de3661cd1a598def6eb6bdd65acd51879f1dc350
Alexey Zalesny <azalesny@griddynamics.com>
2019-03-21 14:42:25 +0400

    Add minimal rabbitmq installation

---
+++ rabbitmq.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+    region = "${var.aws_region}"
+}
+
+resource "aws_instance" "rabbitmq_instance" {
+  ami = "${var.mysql_ami}"
+  instance_type = "${var.mysql_instance_type}"
+  key_name = "${var.ssh_key_pair}"
+  vpc_security_group_ids = ["${var.security_groups}"]
+  subnet_id = "${var.subnet_id}"
+  private_ip = "${var.ip_addr}"
+  associate_public_ip_address = true
+
+  provisioner "local-exec" {
+    command = "ansible-playbook -b -i ${var.ip_addr}, -u centos -v playbooks/rabbitmq.yml"
+  }
+
+  root_block_device {
+    volume_size = "${var.mysql_disk_size}"
+  }
+ 
+  tags {
+    Name = "rabbitmq-ssl01"
+    Project = "${var.project_name}"
+  }
+
+}
