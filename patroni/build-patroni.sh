51e03b5ca436be84197de6be5a448fa13a0461c7
Alexey Zalesny <azalesny@griddynamics.com>
2019-07-08 11:10:10 +0200

    Switch to PostgreSQL from the official repository

--- patroni/build-patroni.sh
+++ patroni/build-patroni.sh
@@ -3,7 +3,7 @@
 # Create Patroni portable distribution on bare RedHat 7
 
 # Install development tools
-sudo yum install -y gcc postgresql-devel python-devel
+sudo yum install -y gcc python-devel
 
 # Add EPEL repository
 sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
@@ -14,10 +14,25 @@ sudo yum install -y python2-pip
 # Install virtualenv
 sudo pip install virtualenv
 
+# Add PostgreSQL repository
+sudo yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+
+# Install PostgreSQL development libs
+sudo yum install -y postgresql96-devel
+
 # Create Patroni environment and switch into it
 virtualenv patroni
 . ./patroni/bin/activate
 
-# Build Patroni and pack it
+# Build Patroni
+export PATH=/usr/pgsql-9.6/bin:$PATH
 pip install patroni[zookeeper]
+
+# Make shebang portable
+for scr in patroni/bin/*
+do
+  sed -i -e "1s%`pwd`/patroni/bin/python2%/usr/bin/env python%" $scr
+done
+
+# Pack Patroni
 tar czf patroni.tgz patroni
