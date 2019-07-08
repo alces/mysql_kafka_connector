339c17a9051f8d1cb9150de9153f23e4b8d815c8
Alexey Zalesny <azalesny@griddynamics.com>
2019-07-08 09:38:09 +0200

    Add Patroni build script

--- patroni/build-patroni.sh
+++ patroni/build-patroni.sh
@@ -4,3 +4,17 @@
 
 # Add EPEL repository
 sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+
+# Install PIP, compiller, and development libraries
+sudo yum install -y python2-pip gcc postgresql-devel python-devel
+
+# Install virtualenv
+sudo pip install virtualenv
+
+# Create Patroni environment and switch into it
+virtualenv patroni
+. ./patroni/bin/activate
+
+# Build Patroni and pack it
+pip install patroni[zookeeper]
+tar czf patroni.tgz patroni
